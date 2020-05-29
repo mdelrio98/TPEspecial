8845efdc969f1bb35947efb9f1f0aceb7d9e1ee0
Matias Del Rio <m.delrio1998@gmail.com>
2020-05-29 17:06:19 -0300

    main.cpp

---
+++ Concesionaria/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <iostream>
+#include <cstddef>
+#include "Concesionaria.h"
+using namespace std;
+
+/**
+ * Abre el archivo según el origen, procesa las líneas del mismo y
+ * almacena la información resultante en el contenedor pasado por referencia.
+ **/
+
+
+int main()
+{
+
+    Concesionaria c;
+    c.procesar_archivo_entrada("prueba.csv");
+    //existe patente
+    c.imprimirarbolautos();
+    cout<<"Hola"<<endl;
+    string s= "QVP111";
+    if(c.existePatente(s)== true){
+        cout<<"Se encuentra la patente"<<endl;
+    }
+    else
+        cout<<"No se encuentra la patente"<<endl;
+    //Listar modelo
+    lista<Auto*>listaPorModelo;
+    c.listarModelo(2011,listaPorModelo);
+//    c.imprimirlista(listaPorModelo);
+    //Mayor precio
+//    Auto a0;
+//    a0 = c.mayorPrecio(arbol_Autos);
+//    delete c;
+    return 0;
+}
